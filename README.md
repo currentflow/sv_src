f9fcf83ec62b355eae8c45f1b0b9b9000d0d1bc5
Rob Leroy <robleroy@msn.com>
2024-11-09 08:13:26 -0600

    update

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 this is a base skeleton project
 
+https://github.com/robertleroy/crate/blob/master/css/reset.css
 
+https://github.com/robertleroy/crate/raw/refs/heads/master/css/reset.css
 
+https://currentflow.github.io/sv_src/static/reset.css
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,9 @@
 <h2 class="">
   {title}
 </h2>
+<input type="text" value={title}>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam voluptatibus repellat, sequi, consequatur quod commodi porro maxime inventore, ipsum autem culpa suscipit aspernatur repudiandae perferendis nemo? Quod expedita natus at?</p>
 
 <style>
 
